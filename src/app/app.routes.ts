e456b01f5903c38bd7c967b3b757e10a3eb84865
GersonTeranGT <guapygt543@gmail.com>
2026-02-10 19:39:20 -0500

    componentes nav, footer y servi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //1. ruta incial
+    {path:'', component:Home},
+    //3. redireccion si el usuario escribe una url no existe
+    //{path:'**', component:Pagina404}
+];
