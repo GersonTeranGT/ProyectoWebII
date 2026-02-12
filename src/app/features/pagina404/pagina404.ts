16d41243ee56c5c93f246f6e5c11cbd8d16c9f5b
GersonTeranGT <guapygt543@gmail.com>
2026-02-11 19:53:55 -0500

    aplicado error 404

---
+++ src/app/features/pagina404/pagina404.ts
@@ -0,0 +1,21 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pagina404',
+  imports: [],
+  templateUrl: './pagina404.html',
+  styleUrl: './pagina404.css',
+})
+export class Pagina404 {
+  urlIntentada: string = '';
+
+  constructor(private location: Location, private router: Router) {
+    this.urlIntentada = this.router.url;  //captura la URL que causó el 404
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
