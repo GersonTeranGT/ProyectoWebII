66d48ce93212ae57267a10327333c518d70b9baa
GersonTeranGT <guapygt543@gmail.com>
2026-02-12 17:42:45 -0500

    mejora del 404 y login

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from "../../shared/hero/hero";
 import { Servicios } from "../../shared/servicios/servicios";
+import { Carrusel } from "../../shared/carrusel/carrusel";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Servicios,],
+  imports: [Hero, Servicios, Carrusel],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
