39a42c374f96d48d35d44a9ef7f86b2a189f92ed
GersonTeranGT <guapygt543@gmail.com>
2026-02-12 20:23:31 -0500

    hero reutilizable

--- src/app/features/historial-test/historial-test.ts
+++ src/app/features/historial-test/historial-test.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TablaTest } from "../../shared/tabla-test/tabla-test";
+import { Hero } from "../../shared/hero/hero";
 
 @Component({
   selector: 'app-historial-test',
-  imports: [TablaTest],
+  imports: [TablaTest, Hero],
   templateUrl: './historial-test.html',
   styleUrl: './historial-test.css',
 })
--- src/app/shared/hero/hero.ts
+++ src/app/shared/hero/hero.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,5 +8,19 @@ import { RouterLink } from '@angular/router';
   styleUrl: './hero.css',
 })
 export class Hero {
-
+  @Input() titulo!: string;
+  @Input() parrafo!: string;
+  @Input() imagenFondo!: string;
+  @Input() altura: string = 'min-h-[90vh]';
+  
+  // Botones - Opcionales
+  @Input() mostrarBoton1: boolean = false;
+  @Input() textoBoton1: string = 'Comienza ahora';
+  @Input() rutaBoton1: string = '/';
+  @Input() colorBoton1: string = 'bg-emerald-600 hover:bg-emerald-700 text-emerald-50';
+  
+  @Input() mostrarBoton2: boolean = false;
+  @Input() textoBoton2: string = 'Iniciar sesión';
+  @Input() rutaBoton2: string = '/login';
+  @Input() colorBoton2: string = 'bg-emerald-50 hover:bg-emerald-100 text-green-700';
 }
